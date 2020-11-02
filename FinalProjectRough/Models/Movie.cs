5d9dd0066d78e0821152f0172bd8032111ca1d61
Joel Nail <58570850+Joel-Nail@users.noreply.github.com>
2020-11-02 11:01:42 -0600

    Add files via upload

---
+++ FinalProjectRough/Models/Movie.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProjectRough.Models
+{
+    public class Movie
+    {
+        // Scalar Properties
+        
+        public int MovieID { get; set; }
+
+        public String Title { get; set; }
+
+        public String Tagline { get; set; }
+
+        public Enum MPAARating { get; set; }
+
+        public DateTime ReleaseYear { get; set; }
+
+        public String Overview { get; set; }
+        
+        public int Runtime { get; set; }
+        
+        public String Actor { get; set; }
+
+        public Decimal AvgRating { get; set; }
+
+        // Navigational Properties
+
+        public List<MovieRating> MovieRatings { get; set; }
+
+        public List<Showing> Showings { get; set; }
+    }
+}
---
+++ FinalProjectRough/Models/MovieRating.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProjectRough.Models
+{
+    public class MovieRating
+    {
+        // Scalar Properties
+
+        public int MovieRatingID { get; set; }
+
+        public Enum NumericalRating { get; set; }
+
+        // what is this?
+        public Enum ReviewStatus { get; set; }
+
+        public String Review { get; set; }
+
+        // Navigational Properties
+
+        public AppUser AppUser { get; set; }
+
+        public Movie Movie { get; set; }
+    }
+}
